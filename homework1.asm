8ac86b5794a46286dfa223c35852778afa0215ec
jimmy Sung <jimmy43333@gmail.com>
2015-04-30 16:46:34 +0800

    finish diamond

--- homework1.asm
+++ homework1.asm
@@ -13,7 +13,7 @@ main:
   li		$v0, 5
   syscall
   move	$s1, $v0
-  addi  $a0,$zero,$s1
+  add  $a0,$zero,$s1
 
   addi	$t0,$zero,1
   beq		$s0, $t0, diamand
@@ -29,12 +29,30 @@ diamond:
   sw  $ra, 0($sp)
   sw  $s0, 4($sp)
   sw  $s1, 8($sp)
+  li  $s0, -1         
+  add  $s1,$zero,$a0  #s1是有幾個空白  
+  li  $s2, 1          #s2是有幾個星號
+  diamondup:  beq $s0,$s1, exitdiamondup
+              jal starandspace
+              addi  $s1,$s1,-1
+              addi  $s2,$s2,2
+              j diamondup
+  exitdiamondup:  addi  $s1,$s1,2
+                  addi  $s2,$s2,-2
+  diamonddown:  beq $s0,$s2, exitdiamonddown
+                jal starandspace
+                addi  $s1,$s1,1
+                addi  $s2,$s2,-2
+                j diamonddown
+  exitdiamonddown:  #把資料還回去
   
-  
+
   
   
 triangle:
 trapezoid:
+starandspace:
+  
 .data
 printout1: .ascliz "Input:\n"
 
